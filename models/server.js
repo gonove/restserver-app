a394974743054023cb6522e2cfcff744e7a476c7
eb_cmgonzalezo <eb_cmgonzalezo@bancoripley.com>
2023-01-15 22:36:38 -0300

    feat:Fin seccion 9

--- models/server.js
+++ models/server.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const cors = require('cors')
 
+const { dbConnection } = require('../database/config')
+
 class Server {
 
     constructor(){
         this.app = express()
         this.port = process.env.PORT
         this.usuariosPath = '/api/usuarios'
-        
+
+        // Conectar a db
+        this.connectDB();
+
         // Middlewares
         this.middlewares()
-        
+
         // Routes
         this.routes()
     }
 
+    async connectDB(){
+        await dbConnection();
+    }
+
     middlewares(){
-        
+
         // CORS
         this.app.use( cors() )
 
@@ -37,7 +46,7 @@ class Server {
     listen(){
         this.app.listen( this.port, () => {
             console.log(`Servidor corriendo en http://localhost:${this.port}`);
-        });        
+        });
     }
 
 }
---
+++ models/usuario.js
@@ -0,0 +1,52 @@
+
+
+const { Schema, model } = require('mongoose')
+
+const UsuarioSchema = Schema({
+
+    nombre : {
+        type: String,
+        required : [ true, 'El nombre es obligatorio']
+    },
+
+    correo : {
+        type : String,
+        required : [true, 'El correo es obligatorio'],
+        unique: true
+    },
+
+    password : {
+        type : String,
+        required : [true, 'La constraseña es obligatoria']
+    },
+
+    image : {
+        type : String,
+    },
+
+    rol : {
+        type : String,
+        required : true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+
+    estado : {
+        type : Boolean,
+        default : true
+    },
+
+    google : {
+        type : Boolean,
+        default : false
+    },
+
+});
+
+UsuarioSchema.methods.toJSON = function() {
+    // Separamos los parametros que recibimos y con el operador spread dejamos 'los demas' en la constante usuario, luego retornamos usuario
+    const { __v, password, ...usuario } = this.toObject();
+
+    return usuario;
+}
+
+module.exports = model( 'Usuario', UsuarioSchema  )
