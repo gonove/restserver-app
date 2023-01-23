14dfd9772a68199652778487f1126d0242ba7bbb
eb_cmgonzalezo <eb_cmgonzalezo@bancoripley.com>
2023-01-23 19:26:34 -0300

    add documentation

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const Server = require('./models/server')
 
+// DOC
+// https://documenter.getpostman.com/view/25389087/2s8ZDYWgrg
+
 // Se crea una nueva instancia de la clase Server que tenemos en models
 const server = new Server()
 
