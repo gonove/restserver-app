1f928203aea0731a54c491d422eefee20794d69c
gonove <gonzalezovelar97@gmail.com>
2023-01-23 19:30:05 -0300

    add doc postman

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Server = require('./models/server')
 
-// DOC
+// Documentation generate for PostMan
 // https://documenter.getpostman.com/view/25389087/2s8ZDYWgrg
 
 // Se crea una nueva instancia de la clase Server que tenemos en models
